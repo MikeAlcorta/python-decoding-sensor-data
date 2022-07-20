75430893c23829c5b53c1689ffcf45a9a5e7382e
MikeAlcorta <miguel.alcorta@associatedbank.com>
2022-07-20 12:07:53 -0500

    module5

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -9,21 +9,29 @@ from particle_count_info import ParticleData
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
-@@ -16,25 +18,50 @@
+data = []
+print("Sensor Data App")
+##############################
+
+# Module 1 code here:
+data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+
 # Module 3 code here:
 temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
 recs = temperature_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Temperature sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
@@ -56,3 +64,4 @@ concentrations = particle_data.get_data_concentrations(data = recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+# Module 5 code here:
